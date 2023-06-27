ca1f6c4b0e5b9c26a40d0106789d47171da98324
Konstantin <DoctorSager163@gmail.com>
2023-06-27 22:07:42 +0300

    [4 Дз]

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        
+    }
+    
+
+}
+
+
+internal partial class Task25
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("введите возводимое в степень число: ");
+        double numberForUp = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("введите степень возведения: ");
+         double upNumber = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine(Pow(numberForUp, upNumber));
+        
+    }
+    static double Pow (double number, double upNumber) 
+    {
+     double resoult = Math.Pow(number, upNumber);
+         return resoult;
+    }
+
+}
+
+
+internal class Task27
+{
+    private static void Main(string[] args)
+    {
+         int a = int.Parse(Console.ReadLine());
+            int s = 0;
+            while (a > 0)
+            {
+ 
+                s = s + a % 10;
+                a = a /10 ;
+ 
+            }
+            Console.WriteLine(s);
+
+    }
+    
+
+}
+*/
