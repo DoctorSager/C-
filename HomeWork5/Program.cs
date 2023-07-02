64ce5897a5899d0e419fcc6a1166d6cce8c53990
Konstantin <DoctorSager163@gmail.com>
2023-07-02 22:59:52 +0300

    HomeWork5

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Concurrent;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double[] array = new double[5];
+        Random rnd = new Random();
+        
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.NextDouble() * (10.00 + 0.01) - 0.01;
+            array[i] = Math.Round(array[i],2);
+        
+            System.Console.WriteLine(array[i]);
+        }
+        double sum  = array.Min() + array.Max();
+        sum = Math.Round(sum,2);
+       System.Console.WriteLine(sum);
+    }
+}
---
+++ HomeWork5/Task36.cs
@@ -0,0 +1,21 @@
+using System.Collections.Concurrent;
+
+/*
+internal class Task36
+{
+    private static void Main(string[] args)
+    {
+        int[] array = new int[4];
+        Random rnd = new Random();
+        
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(-100,100);
+            System.Console.WriteLine(array[i]);
+        }
+        int sum  = array[1] + array[3];
+       System.Console.WriteLine(sum);
+    }
+}
+*/
