777a1a92a4b683e7abad6d58c3e0455ee6e55b80
Konstantin <DoctorSager163@gmail.com>
2023-07-12 20:26:19 +0300

    HomeWork8

---
+++ homeWork8/Task56.cs
@@ -0,0 +1,35 @@
+
+/*
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] matrix = {{3,4,5},
+                 {2,3,4},
+                 {7,4,2},
+                 {2,3,7}};
+
+        int sum = 0;
+        int index = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            int temp = 0;
+            for (int j = 0; j < 4; j++)
+            {
+                temp += matrix[j, i];
+            }
+            if (temp > sum)
+            {
+                sum = temp;
+                index = i;
+            }
+        }
+        Console.WriteLine("Столбец: ");
+        for (int i = 0; i < 4; i++)
+        {
+            Console.WriteLine(matrix[i, index]);
+
+        }
+    }
+}
+*/
